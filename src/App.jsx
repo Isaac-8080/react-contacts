824f84c39a01873631aafc2cc3fd9d61b76d3e98
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-03 04:48:01 +0000

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import './App.css'
 import ContactForm from './components/ContactForm'
 
 function App() {
-  
+
+  const [formData, setFormData] = ([]);
+
+  const handleFormData = (data) => {
+    setFormData([...formData, data])
+  }
+
   return (
     <>
       <div className='flex flex-col items-center mt-5'>
-        <ContactForm />
+        <ContactForm handleFormData={handleFormData} />
       </div>
     </>
   )
