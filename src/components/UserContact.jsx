fce27d98e62f88d7a0e949d51fae4cbab8339bdb
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-11 05:46:53 +0000

    UserContact updated

--- src/components/UserContact.jsx
+++ src/components/UserContact.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import EditForm from './EditForm';
 
-const UserContact = (props) => {
+const UserContact = ({formData, handleEdit, handleDelete }) => {
+  
   return (
     <>
-
-    {props.formData.map((user, index) => {
-      return (
-        <div key={index} className="card bg-slate-500 text-white w-96 shadow">
-          <div className="card-body p-3">
-            <div className='flex gap-3'>
-              <img src={user.image} className='w-14 h-14 object-cover bg-[#F2F2F2] rounded-full' alt="" />
-              <div>
-                <h2 className="card-title">{user.name}</h2>
-                <p>{user.tel}</p>
+      {formData.map((user) => {
+        
+        return (
+          <div key={user.id} className="card bg-slate-500 mt-3 text-white w-96 shadow">
+            <div className="card-body p-3">
+              <div className='flex items-center justify-between gap-3'>
+                <div className='flex gap-3'>
+                  <img src={user.image} className='w-14 h-14 object-cover bg-[#F2F2F2] rounded-full' alt="" />
+                  <div>
+                    <h2 className="card-title">{user.name}</h2>
+                    <p>{user.tel}</p>
+                  </div>
+                </div>
+                <div className='flex gap-2'>
+                  <EditForm user={user} handleEdit={handleEdit} />
+                  <button onClick={() => handleDelete(user.id)} className="bg-red-600 px-2 border-none rounded-lg py-1 h-fit">Delete</button>
+                </div>
               </div>
-              <button className="btn">Edit</button>
-              <button className="btn">Delete</button>
             </div>
           </div>
-        </div>
-      )
-    })}
-      
+        )
+      })}
     </>
-  )
+    
+  );
 }
 
-export default UserContact
+export default UserContact;
