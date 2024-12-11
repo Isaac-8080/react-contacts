6d62fda2a7bf308c1edc8c1ce34a47d5f720b1e2
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-11 05:52:23 +0000

    EditForm updated

---
+++ src/components/EditForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const EditForm = ({user, handleEdit}) => {
+  
+  const [name, setName] = useState(user.name);
+  const [tel, setTel] = useState(user.tel);
+  const [image, setImage] = useState(user.image);
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  
+  const openModal = () =>{
+    setIsModalOpen(true)
+  }
+  
+  const closeModal = () =>{
+    setIsModalOpen(false)
+  }
+
+  
+
+  return (
+    <div>
+      <button className="bg-green-600 px-2 border-none rounded-lg py-1 h-fit" onClick={openModal} >Edit</button>
+      
+      {isModalOpen && (<dialog open id="my_modal_2" className="modal bg-white opacity-50">
+        <div className="modal-box w-fit">
+          <form onSubmit={handleSubmit}>
+            <h1 className='font-bold text-3xl text-black'>Contact Form</h1>
+            <div className='flex flex-col gap-3 mt-3 w-96'>
+              <label className="input input-bordered flex items-center gap-2">
+                <input type="text" className="grow text-black" value={name} onChange={handleName} placeholder="Name" />
+              </label>
+              <label className="input input-bordered flex items-center gap-2">
+                <input type="tel" className="grow text-black" value={tel} onChange={handleTel} placeholder="Tel" />
+              </label>
+              <label className="input input-bordered flex items-center gap-2">
+                <input type="text" className="grow text-black" value={image} onChange={handleImage} placeholder="Image link" />
+              </label>
+              <div className="flex flex-row gap-3 items-center">
+                <button onClick={closeModal} className="btn ">Cancel</button>
+                <button className="btn bg-green-500">Save Changes</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </dialog>)}
+
+    </div>
+  );
+}
+
+export default EditForm;
