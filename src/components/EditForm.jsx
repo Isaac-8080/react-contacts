d6986c72da8b3b90a1848bca6ef5088a38dc160c
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-12 11:40:46 +0000

    EditForm updated

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -32,13 +32,9 @@ const EditForm = ({user, handleEdit}) => {
     e.preventDefault();
 
     // current states
-    const editFormDetails = {name, tel, image, id: user.id}    
-
-    handleEdit(user.id, editFormDetails);
+    const editFormDetails = {image, name, tel, id: user.id}
     
-    setName("");
-    setTel("");
-    setImage("");
+    handleEdit(user.id, editFormDetails);
     
     closeModal();
 
@@ -48,28 +44,30 @@ const EditForm = ({user, handleEdit}) => {
     <div>
       <button className="bg-green-600 px-2 border-none rounded-lg py-1 h-fit" onClick={openModal} >Edit</button>
       
-      {isModalOpen && (<dialog open id="my_modal_2" className="modal bg-white opacity-50">
-        <div className="modal-box w-fit">
-          <form onSubmit={handleSubmit}>
-            <h1 className='font-bold text-3xl text-black'>Contact Form</h1>
-            <div className='flex flex-col gap-3 mt-3 w-96'>
-              <label className="input input-bordered flex items-center gap-2">
-                <input type="text" className="grow text-black" value={name} onChange={handleName} placeholder="Name" />
-              </label>
-              <label className="input input-bordered flex items-center gap-2">
-                <input type="tel" className="grow text-black" value={tel} onChange={handleTel} placeholder="Tel" />
-              </label>
-              <label className="input input-bordered flex items-center gap-2">
-                <input type="text" className="grow text-black" value={image} onChange={handleImage} placeholder="Image link" />
-              </label>
-              <div className="flex flex-row gap-3 items-center">
-                <button onClick={closeModal} className="btn ">Cancel</button>
-                <button className="btn bg-green-500">Save Changes</button>
+      {isModalOpen && (
+        <dialog open id="my_modal_2" className="modal bg-white opacity-50">
+          <div className="modal-box w-fit">
+            <form onSubmit={handleSubmit}>
+              <h1 className='font-bold text-3xl text-black'>Contact Form</h1>
+              <div className='flex flex-col gap-3 mt-3 w-96'>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input type="text" className="grow text-black" value={name} onChange={handleName} placeholder="Name" />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input type="tel" className="grow text-black" value={tel} onChange={handleTel} placeholder="Tel" />
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input type="text" className="grow text-black" value={image} onChange={handleImage} placeholder="Image link" />
+                </label>
+                <div className="flex flex-row gap-3 items-center">
+                  <button onClick={closeModal} className="btn ">Cancel</button>
+                  <button className="btn bg-green-500">Save Changes</button>
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
-      </dialog>)}
+            </form>
+          </div>
+        </dialog>
+      )}
 
     </div>
   );
